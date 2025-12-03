4de1626c135d9f5451468bbc399509a13266309a
Jorge Matos <jisapjisap@gmail.com>
2025-12-03 17:12:00 +0100

    feat: Add `TitleWithDot` component for displaying text with a decorative dot.

--- lib/TitleWithDot.tsx
+++ lib/TitleWithDot.tsx
@@ -7,7 +7,6 @@ interface TitleWithDotProps {
 export const TitleWithDot = ({ text }: TitleWithDotProps) => {
   return (
     <div className="w-full lg:w-1/3 title pt-8">
-      {/* Se añade 'relative' para posicionar el punto y 'pl-10' para darle espacio */}
       <span className="relative inline-block rounded-full border border-gray-400 py-0 pl-8 pr-6 GolosText uppercase font-bold">
         {/* Este span crea el punto decorativo con clases de Tailwind */}
         <span className="absolute left-4 top-1/2 h-2 w-2 -translate-y-1/2 rounded-full bg-prim"></span>
