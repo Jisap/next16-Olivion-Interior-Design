1b7eb9a3586fddd558ecd4dcba88da700ae6aa6e
Jorge Matos <jisapjisap@gmail.com>
2025-12-03 17:39:22 +0100

    feat: add About section UI component with images, text, and animated statistics.

--- app/UI-Components/Index/About/page.tsx
+++ app/UI-Components/Index/About/page.tsx
@@ -5,7 +5,34 @@ import AboutImg1 from "@/public/images/About-1.jpg"
 import AboutImg2 from "@/public/images/About-2.jpg"
 import AboutImg3 from "@/public/images/About-3.jpg"
 import { TitleWithDot } from "../../../../lib/TitleWithDot"
+import CountUp from "react-countup"
 
+const statsData = [
+  {
+    value: 18,
+    suffix: "",
+    title: "Years experience",
+    desc: "Improving homes with expert craftsmanship for year"
+  },
+  {
+    value: 190,
+    suffix: "+",
+    title: "Projects completed",
+    desc: "Over 250 successful projects delivered with quality and care"
+  },
+  {
+    value: 260,
+    suffix: "+",
+    title: "Skilled Tradespeople",
+    desc: "Our team of 30 experts ensures top-quality result"
+  },
+  {
+    value: 328,
+    suffix: "+",
+    title: "Client satisfaction",
+    desc: "IAll of our clients are satisfied with our work"
+  }
+]
 
 export const About = () => {
   return (
@@ -46,6 +73,36 @@ export const About = () => {
         </div>
       </div>
 
+      <div className="px-[8%] lg:px-[12%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {statsData.map((item, index) => (
+          <div key={index} className="about-card">
+            <h2 className="text-5xl tracking-wider CalSans font-bold mb-6">
+              <CountUp
+                start={0}
+                end={item.value}
+                duration={2.5}
+                enableScrollSpy
+                scrollSpyOnce
+              >
+                {({ countUpRef }) => (
+                  <span ref={countUpRef} />
+                )}
+              </CountUp>
+              {item.suffix}
+            </h2>
+
+            <div className="about-content py-6 border-t border-gray-400">
+              <h3 className="mb-3 text-2xl CalSans">
+                {item.title}
+              </h3>
+
+              <p className="text-gray-400 GolosText">
+                {item.desc}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
