8905d62c043d5a8f030a6e7f0721dbeb2718f1bd
Jorge Matos <jisapjisap@gmail.com>
2025-12-06 15:36:45 +0100

    feat: add Footer component with navigation, contact, and social links

--- app/Components/Footer/Footer.tsx
+++ app/Components/Footer/Footer.tsx
@@ -1,4 +1,7 @@
 import Link from "next/link"
+import { BsInstagram, BsTwitter, BsYoutube } from "react-icons/bs"
+import { FaFacebookF } from "react-icons/fa"
+import { ImInstagram } from "react-icons/im"
 
 
 
@@ -50,7 +53,7 @@ export const Footer = () => {
                 Our Team
               </Link>
               <Link
-                href="/UI-Components/Pages/Blogs"
+                href="/UI-Components/Blogs"
                 className="text-gray-300 GolosText mb-1 text-xl transition-all duration-300 hover:text-prim hover:ml-2"
               >
                 Blog
@@ -63,6 +66,85 @@ export const Footer = () => {
               </Link>
             </ul>
           </div>
+
+          <div className="footer-content py-3">
+            <ul className="footer-link flex flex-col">
+              <Link
+                href="/UI-Components/Projects"
+                className="text-gray-300 GolosText mb-1 text-xl transition-all duration-300 hover:text-prim hover:ml-2"
+              >
+                Our Projects
+              </Link>
+              <Link
+                href="/"
+                className="text-gray-300 GolosText mb-1 text-xl transition-all duration-300 hover:text-prim hover:ml-2"
+              >
+                Partners
+              </Link>
+              <Link
+                href="/"
+                className="text-gray-300 GolosText mb-1 text-xl transition-all duration-300 hover:text-prim hover:ml-2"
+              >
+                Partners Program
+              </Link>
+              <Link
+                href="/"
+                className="text-gray-300 GolosText mb-1 text-xl transition-all duration-300 hover:text-prim hover:ml-2"
+              >
+                Affiliate Program
+              </Link>
+              <Link
+                href="/"
+                className="text-gray-300 GolosText mb-1 text-xl transition-all duration-300 hover:text-prim hover:ml-2"
+              >
+                Terms & Conditions
+              </Link>
+              <Link
+                href="/UI-Components/Pages/Contact"
+                className="text-gray-300 GolosText mb-1 text-xl transition-all duration-300 hover:text-prim hover:ml-2"
+              >
+                Support Center
+              </Link>
+            </ul>
+          </div>
+
+          <div className="footer-content py-3 flex flex-col">
+            <h2 className="text-3xl text-prim underline CalSans mb-3">
+              (509) 222-2222
+            </h2>
+
+            <h4 className="text-2xl text-gray-300 underline GolosText mb-3">
+              support@olivioninterior.com
+            </h4>
+
+            <div className="footer-social flex gap-3 py-6 cursor-pointer">
+              <p className="text-gray-300 GolosText transition-all duration-300 hover:text-prim font-semibold hover:-translate-y-1 size-4">
+                <FaFacebookF />
+              </p>
+              <p className="text-gray-300 GolosText transition-all duration-300 hover:text-prim font-semibold hover:-translate-y-1 size-4">
+                <BsInstagram />
+              </p>
+              <p className="text-gray-300 GolosText transition-all duration-300 hover:text-prim font-semibold hover:-translate-y-1 size-4">
+                <BsTwitter />
+              </p>
+              <p className="text-gray-300 GolosText transition-all duration-300 hover:text-prim font-semibold hover:-translate-y-1 size-4">
+                <BsYoutube />
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="footer-bottom flex flex-col justify-center items-center py-8 border-t border-gray-500">
+          <p className="text-gray-300 text-lg GolosText">
+            © 2025 Olivion Interior. All rights reserved.
+          </p>
+
+          <Link
+            href="/https://www.privacypolicyonline.com/"
+            className="transition-all duration-300 text-white text-sm  text-center hover:text-prim"
+          >
+            Privacy Policy | Terms & Conditions
+          </Link>
         </div>
       </div>
     </>
