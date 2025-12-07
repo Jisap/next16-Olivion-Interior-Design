5c1a4d8d0c07adda2cadb20c91f71c19b873b9cc
Jorge Matos <jisapjisap@gmail.com>
2025-12-07 13:30:01 +0100

    feat: add dynamic project details page with data fetching and UI rendering for individual projects.

--- app/UI-Components/Projects/[id]/page.tsx
+++ app/UI-Components/Projects/[id]/page.tsx
@@ -1,9 +1,156 @@
-import React from 'react'
+"use client"
 
-const page = () => {
+import Link from "next/link";
+import ProjectsData from '@/app/JsonData/Projects.json';
+import { useParams } from "next/navigation";
+import Image from "next/image";
+
+const ProjectDetails = () => {
+
+  const { id } = useParams();
+
+  const project = ProjectsData.find((project) => project.id === id);
+  if (!project) {
+    return (
+      <div className="section-bg text-white flex flex-col">
+        <h1 className="text-5xl md:text-6xl GolosText mt-15">
+          Project not found
+        </h1>
+
+        <div className="flex items-center gap-5 mt-3">
+          <Link
+            href="/UI-Components/Projects"
+            className="hover:text-prim text-xl transition-all duration-300 mt-5 border border-gray-300 px-4 py-2 rounded hover:bg-white hover:text-prim"
+          >
+            Back to Projects
+          </Link>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div>page</div>
+    <>
+      {/* Banner superior de la sección */}
+      <div className='section-bg text-white flex flex-col'>
+        <h1 className='text-6xl md:text-8xl GolosText nt-15'>
+          Projects Details
+        </h1>
+
+        <div className='flex items-center text-xl mt-3'>
+          <Link href='/' className='hover:text-prim transition-all duration-300'>
+            Home
+          </Link>
+
+          <i className='ri-arrow-right-wide-fill mt-1' />
+
+          <Link href='/UI-Components/Projects' className='hover:text-prim transition-all duration-300'>
+            Projects
+          </Link>
+
+          <i className='ri-arrow-right-wide-fill mt-1' />
+
+          <h2 className='GolosText'>
+            {project.title}
+          </h2>
+        </div>
+      </div>
+
+      {/* Project Details */}
+      <div className="px-[8%] lg:px-[12%] py-20">
+        <h2 className="CalSans text-5xl md:text-7xl">
+          {project.title}
+        </h2>
+
+        <div className="mt-10 border-b border-gray-300 pb-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-5">
+          <div className="flex flex-col items-start">
+            <h2 className="CalSans text-gray-400 tracking-wider">
+              Architect:
+            </h2>
+
+            <h3 className="GolosText text-xl">
+              Themelexus
+            </h3>
+          </div>
+
+          <div className="flex flex-col items-start">
+            <h2 className="CalSans text-gray-400 tracking-wider">
+              Project Type:
+            </h2>
+
+            <h3 className="GolosText text-xl flex flex-wrap">
+              {project.tags.map((tag, idx) => (
+                <div key={idx} className="flex">
+                  {tag}
+                </div>
+              ))}
+            </h3>
+          </div>
+
+          <div className="flex flex-col items-start">
+            <h2 className="CalSans text-gray-400 tracking-wider">
+              Client:
+            </h2>
+
+            <h3 className="GolosText text-xl">
+              David Oswald
+            </h3>
+          </div>
+
+          <div className="flex flex-col items-start">
+            <h2 className="CalSans text-gray-400 tracking-wider">
+              Terms:
+            </h2>
+
+            <h3 className="GolosText text-xl">
+              6 Months
+            </h3>
+          </div>
+
+          <div className="flex flex-col items-start">
+            <h2 className="CalSans text-gray-400 tracking-wider">
+              Strategy:
+            </h2>
+
+            <h3 className="GolosText text-xl">
+              Minimalistic
+            </h3>
+          </div>
+
+          <div className="flex flex-col items-start">
+            <h2 className="CalSans text-gray-400 tracking-wider">
+              Date:
+            </h2>
+
+            <h3 className="GolosText text-xl">
+              {project.date}
+            </h3>
+          </div>
+        </div>
+
+        <div className="project-det-img mt-10 w-full h-[500px] md:h-[700px] rounded-2xl overflow-hidden">
+          <Image
+            src={project.image}
+            alt={project.title}
+            width={800}
+            height={400}
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        {/* Project info */}
+        <div className="mt-10">
+          <h2 className="CalSans text-5xl md:text-7xl">
+            Design in Details
+          </h2>
+
+          <p className="GolosText text-gray-500 mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, neque animi! Laudantium ullam aliquam tempore ut dolorum vitae sint inventore sequi id aliquid. Tempora repellendus magnam, quaerat minus molestiae quos.
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default page
+export default ProjectDetails
