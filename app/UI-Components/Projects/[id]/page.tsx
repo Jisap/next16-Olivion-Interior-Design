e6956d5b0b9c8e1bcf6abaf1fe2b07b0a43ad7db
Jorge Matos <jisapjisap@gmail.com>
2025-12-07 16:02:15 +0100

    feat: Add project data and a dynamic project detail page component.

--- app/UI-Components/Projects/[id]/page.tsx
+++ app/UI-Components/Projects/[id]/page.tsx
@@ -3,15 +3,29 @@
 import Link from "next/link";
 import ProjectsData from '@/app/JsonData/Projects.json';
 import { useParams } from "next/navigation";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
+import CountUp from "react-countup";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/autoplay";
+import Gallery1 from "@/public/images/gallery-1.jpg"
+import Gallery2 from "@/public/images/gallery-2.jpg"
+import Gallery3 from "@/public/images/gallery-3.jpg"
+import Gallery4 from "@/public/images/gallery-4.jpg"
+import Gallery5 from "@/public/images/gallery-5.jpg"
+import Gallery6 from "@/public/images/gallery-6.jpg"
 
-
+const Gallery: StaticImageData[] = [
+  Gallery1, Gallery2, Gallery3, Gallery4, Gallery5, Gallery6
+];
 
 const ProjectDetails = () => {
 
   const { id } = useParams();
 
   const project = ProjectsData.find((project) => project.id === id);
+
   if (!project) {
     return (
       <div className="section-bg text-white flex flex-col">
@@ -22,7 +36,7 @@ const ProjectDetails = () => {
         <div className="flex items-center gap-5 mt-3">
           <Link
             href="/UI-Components/Projects"
-            className="hover:text-prim text-xl transition-all duration-300 mt-5 border border-gray-300 px-4 py-2 rounded hover:bg-white hover:text-prim"
+            className="hover:text-prim text-xl transition-all duration-300 mt-5 border border-gray-300 px-4 py-2 rounded hover:bg-white"
           >
             Back to Projects
           </Link>
@@ -30,6 +44,11 @@ const ProjectDetails = () => {
       </div>
     )
   }
+
+  const galleryImages: string[] | StaticImageData[] =
+    project.gallery && project.gallery.length > 0
+      ? project.gallery
+      : Gallery;
 
   return (
     <>
@@ -157,12 +176,87 @@ const ProjectDetails = () => {
                 <p className="GolosText text-gray-500">
                   <span className="font-bold text-black">{info.title}</span> {info.pere}
                 </p>
-
-
               </div>
             ))}
           </div>
         </div>
+
+        <div className="mt-10 border border-gray-200 rounded-md grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+          {project.metrics?.map((metric) => (
+            <div key={metric.id} className="p-10 border border-gray-200 flex flex-col justify-center items-center">
+              <h2 className="GoloxText text-5xl tracking-wider font-bold">
+                (
+                <CountUp
+                  start={0}
+                  end={metric.value}
+                  duration={8}
+                  enableScrollSpy
+                  scrollSpyOnce
+                >
+                  {({ countUpRef }) => (
+                    <span ref={countUpRef} />
+                  )}
+                </CountUp>
+                {metric.unit}
+                )
+              </h2>
+
+              <h2 className="GolosText text-xl mt-2">
+                {metric.label}
+              </h2>
+            </div>
+          ))}
+        </div>
+
+        <div className="mt-10">
+          <h2 className="CalsSans text-5xl md:text-7xl">
+            Incredible Result
+          </h2>
+
+          <p className="text-gray-500 GolosText mt-5">
+            Establishg multi-sensory experiences, we can design interiors thar resonate across ages ande demographics.
+            These rooms and spaces connects us to narure as proven way to inspire us, boost our productivity, and create
+            greater well-being. Beyond these benefits, by reducing stress and enhancing creativity, we can aslo expedite
+            healing.
+          </p>
+        </div>
+      </div>
+
+      {/* Project Gallery */}
+      <div className="project-gallery">
+        <Swiper
+          slidesPerView={2}
+          spaceBetween={0}
+          loop={true}
+          modules={[Autoplay]}
+          autoplay={{
+            delay: 1500,
+          }}
+          speed={1800}
+          breakpoints={{
+            1200: {
+              slidesPerView: 2,
+            },
+            575: {
+              slidesPerView: 1,
+            },
+            0: {
+              slidesPerView: 1,
+            },
+          }}
+        >
+          {galleryImages.map((item, index) => (
+            <SwiperSlide key={index}>
+              <Image
+                src={item}
+                alt={`Gallery-${index}`}
+                width={800}
+                height={400}
+                className="w-full h-[600px] object-cover"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </>
   )
