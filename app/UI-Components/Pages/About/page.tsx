cea872f165f5e3820644bfd5d88ccbf2e3573415
Jorge Matos <jisapjisap@gmail.com>
2025-12-08 19:00:26 +0100

    feat: add About Us page component

--- app/UI-Components/Pages/About/page.tsx
+++ app/UI-Components/Pages/About/page.tsx
@@ -1,6 +1,12 @@
+"use client"
+
 import { TitleWithDot } from '@/lib/TitleWithDot'
 import Link from 'next/link'
-import React from 'react'
+import Image from 'next/image'
+
+import About2 from '@/public/images/About-2.jpg'
+import About4 from '@/public/images/About-4.jpg'
+
 
 const About = () => {
   return (
@@ -26,15 +32,54 @@ const About = () => {
 
       <div className='px-[8%] lg:px-[12%] py-20'>
         <div className="flex flex-col lg:flex-row gap-10">
-          <TitleWithDot text="About Olivion" />
+          <div className='w-full lg:w-2/3 pt-8'>
+            <span className='rounded-full title-span border border-gray-400 px-6 GolosText uppercase font-bold'>
+              Started In 1989
+            </span>
 
-          <div className="w-full lg:w-2/3">
-            <h1 className="CalSans text-4xl md:text-7xl">
+            <h1 className="CalSans pt-5 text-4xl md:text-5xl w-full lg:w-[80%] mb-5">
               We Shape <span className="text-prim">Interior Designs, Crafting</span> Timeless and Inspiring Spaces
             </h1>
+
+            <div className='about-content md:pt-10'>
+              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5'>
+                <div className='about-text'>
+                  <h2 className='text-[10rem] leading-40 GolosText font-bold text-prim '>
+                    26
+                  </h2>
+
+                  <p className='text-2xl lg:px-15 CalSans'>
+                    Years of Experience
+                  </p>
+                </div>
+
+                <div className='about-img'>
+                  <Image
+                    src={About4}
+                    alt="About4"
+                    width={200}
+                    height={200}
+                    className='object-cover rounded-2xl h-full w-full'
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='w-full lg:w-1/3'>
+            <div className='about-img md:h-[600px]'>
+              <Image
+                src={About2}
+                alt="About2"
+                width={200}
+                height={200}
+                className='object-cover rounded-2xl h-full w-full'
+              />
+            </div>
           </div>
         </div>
       </div>
+
     </>
   )
 }
