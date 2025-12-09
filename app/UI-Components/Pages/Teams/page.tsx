d3d1da73cde65cdb64c15e67881bad30e053e192
Jorge Matos <jisapjisap@gmail.com>
2025-12-09 17:41:40 +0100

    feat: add Teams page component with banner and navigation.

--- app/UI-Components/Pages/Teams/page.tsx
+++ app/UI-Components/Pages/Teams/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+"use client"
 
-const page = () => {
+import Link from "next/link"
+
+const Teams = () => {
   return (
-    <div>page</div>
+    <>
+      {/* Banner superior de la sección */}
+      <div className='section-bg text-white flex flex-col'>
+        <h1 className='text-8xl GolosText mt-15'>
+          Our Team
+        </h1>
+
+        <div className='flex items-center text-xl mt-3'>
+          <Link href='/' className='hover:text-prim transition-all duration-300'>
+            Home
+          </Link>
+
+          <i className='ri-arrow-right-wide-fill mt-1' />
+
+          <h2 className='GolosText'>
+            Teams
+          </h2>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default page
+export default Teams
