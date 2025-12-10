ba484ccd82d7c1ffc448a304ed99c1366a951418
Jorge Matos <jisapjisap@gmail.com>
2025-12-10 17:41:29 +0100

    feat: add contact page UI component.

--- app/UI-Components/Pages/Contact/page.tsx
+++ app/UI-Components/Pages/Contact/page.tsx
@@ -148,6 +148,17 @@ const Contact = () => {
           </div>
         </div>
       </div>
+
+      <div className="contact-map h-[600px]">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d118989.2671982148!2d-3.7075854696038806!3d40.413556791274296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2ses!4v1765384665366!5m2!1ses!2ses"
+          className="w-full rounded-2xl brightness-100 grayscale"
+          width="100%"
+          height="100%"
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </div>
     </>
   )
 }
